3ccd13546dfb0f85aa2df6b9cd6769cb041aa38b
Diksha <diksha.wakode123@gmail.com>
2023-08-13 10:59:33 +0530

    firstStage

--- Courses_backend/src/main/java/com/courses/backend/CoursesBackendApplication.java
+++ Courses_backend/src/main/java/com/courses/backend/CoursesBackendApplication.java
@@ -2,7 +2,9 @@ package com.courses.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//@SpringBootApplication
 
 @SpringBootApplication
 public class CoursesBackendApplication {
